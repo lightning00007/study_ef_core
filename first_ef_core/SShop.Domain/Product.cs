9e2e41b389e1e7345cce3ccd121a669492e404f8
lightning <belowmypaygrade@gmail.com>
2026-06-21 00:08:31 +0700

    Add domain classes

---
+++ first_ef_core/SShop.Domain/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SShop.Domain
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int OriginalPrice { get; set; } = 0;
+        public int ReducedPrice { get; set; } = 0;
+
+        public List<Specification> Specs { get; set; } = new List<Specification>();
+    }
+}
---
+++ first_ef_core/SShop.Domain/Specification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SShop.Domain
+{
+    public class Specification
+    {
+        public int Id { get; set; }
+        public string Type { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string LocalName { get; set; } = string.Empty;
+        public int ProductId { get; set; }
+        public Product? Product { get; set; }
+    }
+}
