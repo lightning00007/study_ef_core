d0fbea74c166c04498ecb292e3ac68243a0f0d66
Pham Phuong Bac <somethingnotright123@outlook.com>
2026-06-20 18:26:29 +0700

    New project

---
+++ first_ef_core/first_ef_core/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace first_ef_core
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
