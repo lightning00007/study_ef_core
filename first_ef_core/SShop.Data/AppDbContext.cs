57ffe25c8b5babbd94c811dac12696884d6d4d95
lightning <belowmypaygrade@gmail.com>
2026-06-20 22:09:08 +0700

    Create 3 layers of EF Core

---
+++ first_ef_core/SShop.Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SShop.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
