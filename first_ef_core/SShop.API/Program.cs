dcaddc64b281f56394e9eeeb752a097cc58cade3
lightning <belowmypaygrade@gmail.com>
2026-06-24 23:55:09 +0700

    Add random data to check sqlite db file

--- first_ef_core/SShop.API/Program.cs
+++ first_ef_core/SShop.API/Program.cs
@@ -13,6 +13,13 @@ builder.Services.AddHttpClient();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.EnsureCreated();
+    DatabaseSeeder.Seed(dbContext);
+}
+
 //go to https://localhost:7067/WeatherForecast to see the result
 
 // Configure the HTTP request pipeline.
---
+++ first_ef_core/SShop.Data/DatabaseSeeder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SShop.Data
+{
+    public class DatabaseSeeder
+    {
+        public static void Seed(AppDbContext context)
+        {
+            // Check if the database is empty
+            if (!context.Products.Any())
+            {
+                // Add initial data to the database
+                var products = new List<Domain.Product>
+                {
+                    new Domain.Product { Name = "Product 1", OriginalPrice = 1000, ReducedPrice = 800, Specs = new List<Domain.Specification>(){new Domain.Specification { Type = "Spec Type 1", Name = "Green",LocalName = "Xanh"} } },
+                    new Domain.Product { Name = "Product 2", OriginalPrice = 1500, ReducedPrice = 1200, Specs = new List<Domain.Specification>(){new Domain.Specification { Type = "Spec Type 2", Name = "Blue",LocalName = "Xanh"} } },
+                    new Domain.Product { Name = "Product 3", OriginalPrice = 700, ReducedPrice = 560, Specs = new List<Domain.Specification>(){new Domain.Specification { Type = "Spec Type 3", Name = "Red",LocalName = "Do"} } }
+                };
+                context.Products.AddRange(products);
+                context.SaveChanges();
+            }
+        }
+    }
+}
